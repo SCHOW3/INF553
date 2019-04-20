00837d66cb025571408747a4aa6638d7d4bcc371
Lucy Wang <wang796@usc.edu>
2019-04-19 22:23:42 -0700

    idk

--- project.py
+++ project.py
@@ -6,19 +6,32 @@ import pandas as pd
 from operator import add
 from collections import defaultdict
 
-from pyspark import SparkContext
+from pyspark.sql import SparkSession
+# from pyspark import SparkContext
+
+
+# if __name__ == "__main__":
+#     if len(sys.argv) < 1:
+#         sys.exit(-1)
+
+#     sc = SparkContext(appName="project")
+
+#     # raw_data = sc.textFile("tmdb_5000_movies.csv")
+#     df = sc.read.csv("tmdb_5000_movies.csv", inferSchema=True, header=True)
+#     df.printSchema()
+#     # header = raw_data.take(1)[0]
+#     # data = raw_data.filter(lambda line: line != header)\
+#     #     .map(lambda line: line.split(","))
+
+#     # print(data.collect())
+    
 
 if __name__ == "__main__":
-    if len(sys.argv) < 1:
-        sys.exit(-1)
-
-    sc = SparkContext(appName="project")
-
-    raw_data = sc.textFile("tmdb_5000_movies.csv")
-    header = raw_data.take(1)[0]
-    data = raw_data.filter(lambda line: line != header)\
-        .map(lambda line: line.split(","))
-
-    print(data.collect())
+    spark = SparkSession.builder.appName('recommender').getOrCreate()
+    df = spark.read.csv("tmdb_5000_movies.csv", inferSchema=True, header=True)
+    # df = df.select(['id', 'title', 'genres', 'keywords', 'popularity'])
+    # df = df.loc([keywords])
+    df = df.select('title', 'genres', 'keywords', 'popularity')
     
+    df.show()
 
